58f34ab3348fba647e3ab57ebcbbedc56ce4c602
Jovil <jovilh@gmail.com>
2024-07-02 01:50:17 +0800

    extend auth- and freeComponent

--- src/AuthComponent.js
+++ src/AuthComponent.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
 
 export default function AuthComponent() {
+  const [message, setMessage] = useState("");
+  const token = cookies.get("TOKEN");
+
+  useEffect(() => {
+    const url = 'https://project-alpha-auth-db-app-b623d85e31d2.herokuapp.com/auth-endpoint';
+
+    // set configurations for the API call here
+    const configuration = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    // make the API call
+    fetch(url, configuration)
+      .then(response => response.json())
+      .then((result) => {
+        // assign the message in our result to the message we initialized above
+        setMessage(result.message);
+      })
+      .catch((error) => {console.log(error)});
+  }, []);
+
+  const logout = () => {
+    // destroy the cookie
+    cookies.remove("TOKEN", { path: "/" });
+    // redirect user to the landing page
+    window.location.href = "/";
+  }
+
   return (
-    <div>
-      <h1 className="text-center">Auth Component</h1>
+    <div className="text-center">
+      <h1>Auth Component</h1>
+      <h3 className="text-danger">{message}</h3>
+
+      {/* logout */}
+      <Button type="submit" variant="danger" onClick={() => logout()}>
+        Logout
+      </Button>
     </div>
   );
 }
--- src/FreeComponent.js
+++ src/FreeComponent.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function FreeComponent() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const url = 'https://project-alpha-auth-db-app-b623d85e31d2.herokuapp.com/free-endpoint';
+
+    const configuration = {
+      method: 'GET',
+    };
+
+    fetch(url, configuration)
+    .then(response => response.json())
+    .then((result) => {
+      setMessage(result.message);
+    })
+    .catch((error) => {
+      error = new Error();
+    });
+  }, [])
+
   return (
     <div>
       <h1 className="text-center">Free Component</h1>
+      <h3 className="text-center text-danger">{message}</h3>
     </div>
   );
 }
