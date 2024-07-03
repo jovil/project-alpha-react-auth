d9683bce8b6a318e8ce59d44e4310d7b565bab64
Jovil <jovilh@gmail.com>
2024-07-03 16:49:31 +0800

    rename component

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,7 @@
 import { Col, Row } from "react-bootstrap";
 import Login from "./Login";
 
-export default function Account() {
+export default function Home() {
   return (
     <Row>
       <Col xs={12} sm={12} md={6} lg={6}>
