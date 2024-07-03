bb612da3aabc93a8c4aca8ebea1f526996b45eef
Jovil <jovilh@gmail.com>
2024-07-03 19:47:05 +0800

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,41 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { GlobalStateContext } from './context';
-import { Container, Col, Row } from "react-bootstrap";
+import { Container, Col, Row, Button, Stack } from "react-bootstrap";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import FreeComponent from "./FreeComponent";
 import ProtectedRoutes from "./ProtectedRoutes";
 import "./App.css";
 import Register from "./Register";
+import Login from './Login';
 
 function App() {
   const { state } = useContext(GlobalStateContext);
 
   return (
     <Container>
-      <Row>
-        <Col className="text-center">
-          <section id="navigation">
+      <Row className="my-10">
+        <Col></Col>
+        <Col className="flex justify-center" xs={6}>
+          <Stack className="gap-x-8" direction="horizontal">
             <Link to="/">Home</Link>
             <Link to="/free">Free Component</Link>
             <Link to="/auth">Auth Component</Link>
+          </Stack>
+        </Col>
+        <Col>
+          <Stack className="flex justify-end" direction="horizontal" gap={3}>
             {!state.isLoggedIn ? (
-              <Link to="/register">Register</Link>
+              <>
+                <Link to="/login">
+                  <Button variant="primary">Login</Button>
+                </Link>
+                <Link to="/register">
+                  <Button variant="outline-dark">Register</Button>
+                </Link>
+              </>
             ) : ''}
-          </section>
+            </Stack>
         </Col>
       </Row>
 
@@ -30,6 +43,7 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path="/free" element={<FreeComponent/>} />
         <Route path="/auth" element={<ProtectedRoutes/>} />
+        <Route path="/login" element={<Login/>} />
         <Route path="/register" element={<Register/>} />
       </Routes>
     </Container>
--- src/Home.tsx
+++ src/Home.tsx
@@ -3,10 +3,8 @@ import Login from "./Login";
 
 export default function Home() {
   return (
-    <Row>
-      <Col xs={12} sm={12} md={6} lg={6}>
-        <Login />
-      </Col>
-    </Row>
+    <>
+      <p>feed</p>
+    </>
   );
 }
