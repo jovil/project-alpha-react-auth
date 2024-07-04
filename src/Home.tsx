e7b0ef9c4c747161491245a7fbbcb449950ee307
Jovil <jovilh@gmail.com>
2024-07-04 20:30:14 +0800

    added create post feature and list all posts on hp

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,50 @@
+import { useEffect, useState } from "react";
+import { useUser } from "./UserContext";
+
 export default function Home() {
+  const { userState } = useUser();
+  const [posts, setPosts] = useState([]);
+  const url = `${process.env.REACT_APP_API_URL}/posts?email=${userState.email}`;
+
+  const configuration = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json", // Specify the content type as JSON
+    },
+  };
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch(url, configuration);
+        const result = await response.json();
+        setPosts(result);
+      } catch (error) {
+        console.log("error creating post", error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <>
-      <p>feed</p>
+      <section className="flex flex-col gap-5">
+        {posts ? (
+          <>
+            {posts?.map((post: any) => {
+              return (
+                <div key={post._id}>
+                  <h2>{post.title}</h2>
+                  <p>{post.description}</p>
+                </div>
+              );
+            })}
+          </>
+        ) : (
+          ""
+        )}
+      </section>
     </>
   );
 }
---
+++ src/pages/AuthPage/AuthComponent.tsx
@@ -0,0 +1,79 @@
+import { useState, useEffect } from "react";
+import { useUser } from "../../UserContext";
+import { useNavigate } from "react-router-dom";
+import { Button, Form } from "react-bootstrap";
+import HeaderSection from "./HeaderSection";
+
+const AuthComponent = () => {
+  const { userState } = useUser();
+  const navigate = useNavigate();
+  const [post, setPost] = useState({
+    email: "",
+    title: "",
+    description: "",
+  });
+  const url = `${process.env.REACT_APP_API_URL}/create`;
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+
+    setPost((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  useEffect(() => {
+    setPost({ ...post, email: userState.email });
+  }, [post.email, userState.email]);
+
+  const handleClick = async (e: any) => {
+    e.preventDefault();
+
+    const configuration = {
+      method: "POST", // Specify the request method
+      headers: {
+        "Content-Type": "application/json", // Specify the content type as JSON
+      },
+      body: JSON.stringify(post), // Convert the data to JSON string
+    };
+
+    try {
+      const response = await fetch(url, configuration);
+      const result = await response.json();
+      navigate("/");
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  return (
+    <>
+      <HeaderSection />
+      <div>
+        <h1 className="title">Create Post</h1>
+        <Form>
+          <Form.Group>
+            <Form.Control
+              name="title"
+              placeholder="Title"
+              onChange={handleChange}
+              value={post.title}
+            />
+            <Form.Control
+              name="description"
+              placeholder="Description"
+              onChange={handleChange}
+              value={post.description}
+            />
+          </Form.Group>
+          <Button onClick={handleClick}>Save</Button>
+        </Form>
+      </div>
+    </>
+  );
+};
+
+export default AuthComponent;
