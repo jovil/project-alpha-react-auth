466964fbf4c19d421b367a0de715648f9cd14283
Jovil <jovilh@gmail.com>
2024-07-05 19:31:24 +0800

    expand tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      white: "#fff",
+      blue: "#0d6efd",
+      "blue-100": "#1fb6ff",
+      dark: "#212529",
+      danger: "#dc3545",
+      purple: "#7e5bef",
+      pink: "#ff49db",
+      orange: "#ff7849",
+      green: "#13ce66",
+      yellow: "#ffc82c",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+      transparent: "transparent",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        "8xl": "96rem",
+        "9xl": "128rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
   },
   plugins: [],
-}
+};
