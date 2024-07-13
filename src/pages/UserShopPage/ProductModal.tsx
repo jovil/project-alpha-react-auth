6cae9ccdef357f01c570d4da8881ab48363044b2
Jovil <jovilh@gmail.com>
2024-07-13 15:02:34 +0800

    style product modal and added swiper

--- src/pages/UserShopPage/ProductModal.tsx
+++ src/pages/UserShopPage/ProductModal.tsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import Swiper from "swiper";
+import "swiper/css";
+import { Thumbs } from "swiper/modules";
+import loading from "../../assets/images/loading.gif";
 
 interface Product {
   productName: string;
@@ -16,7 +20,38 @@ const ProductModal = ({
   productId: any;
   onToggleModal: any;
 }) => {
+  Swiper.use([Thumbs]);
   const [product, setProduct] = useState<Product | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const initSwiper = () => {
+    const swiperThumbs = new Swiper(".swiper-thumbs", {
+      spaceBetween: 12,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true,
+    });
+
+    new Swiper(".swiper", {
+      spaceBetween: 12,
+      speed: 400,
+      slidesPerView: 1,
+      initialSlide: 0,
+      allowTouchMove: false,
+      on: {
+        init: function () {
+          setIsLoading(false);
+        },
+      },
+      thumbs: {
+        swiper: swiperThumbs,
+      },
+    });
+  };
+
+  useEffect(() => {
+    initSwiper();
+  }, [product]);
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -58,34 +93,83 @@ const ProductModal = ({
           className="h-full w-2/5 overflow-scroll ml-auto"
           onClick={(e) => e.stopPropagation()}
         >
-          <div className="min-h-[calc(100vh-16px)] m-2 bg-white rounded p-2 flex flex-col gap-3">
+          <div className="min-h-[calc(100vh-16px)] m-2 bg-white rounded p-3 pb-16 flex flex-col gap-3 relative">
             {product ? (
-              <>
-                <div>
-                  {product.fileUrl.toReversed().map((url, urlIndex) => (
-                    <img
-                      className="rounded w-full"
-                      key={urlIndex}
-                      src={url}
-                      alt=""
-                    />
-                  ))}
+              <div className="flex flex-col gap-6">
+                <div className="flex flex-col gap-3">
+                  <div className="swiper m-0">
+                    <div className="swiper-wrapper">
+                      {product.fileUrl.toReversed().map((url, urlIndex) => (
+                        <div
+                          className="swiper-slide aspect-square flex justify-center items-center"
+                          key={urlIndex}
+                        >
+                          {isLoading ? (
+                            <img className="w-6 h-6" src={loading} alt="" />
+                          ) : (
+                            <img
+                              className="rounded object-cover w-full"
+                              key={urlIndex}
+                              src={url}
+                              alt=""
+                            />
+                          )}
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                  <div className="swiper-thumbs">
+                    <div className="swiper-wrapper">
+                      {product.fileUrl.toReversed().map((url, urlIndex) => (
+                        <div
+                          className="swiper-slide cursor-pointer"
+                          key={urlIndex}
+                        >
+                          {isLoading ? (
+                            <img className="w-6 h-6" src={loading} alt="" />
+                          ) : (
+                            <img
+                              className="rounded object-cover w-full"
+                              key={urlIndex}
+                              src={url}
+                              alt=""
+                            />
+                          )}
+                        </div>
+                      ))}
+                    </div>
+                  </div>
                 </div>
 
-                <h2 className="text-xl">{product.productName}</h2>
-                <p className="text-dark/80">{product.productDescription}</p>
-                <p className="text-md">RM {product.productPrice}</p>
-                <div>
-                  <NavLink
-                    to={product.paymentLink}
-                    className="btn-primary inline-block"
-                  >
-                    Buy now
-                  </NavLink>
+                <div className="flex flex-col gap-14">
+                  <div className="flex flex-col gap-2">
+                    <h2 className="text-2xl">{product.productName}</h2>
+                    <p className="text-xl">RM {product.productPrice}</p>
+                  </div>
+
+                  <div>
+                    <NavLink
+                      to={product.paymentLink}
+                      className="btn-primary block text-center py-2.5"
+                    >
+                      Buy now
+                    </NavLink>
+                  </div>
                 </div>
-              </>
+
+                {product.productDescription && (
+                  <div className="flex flex-col gap-1">
+                    <p className="font-medium">Description</p>
+                    <p className="text-dark/80">{product.productDescription}</p>
+                  </div>
+                )}
+              </div>
             ) : (
-              <p>Loading...</p>
+              <img
+                className="w-6 h-6 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                src={loading}
+                alt=""
+              />
             )}
           </div>
         </div>
