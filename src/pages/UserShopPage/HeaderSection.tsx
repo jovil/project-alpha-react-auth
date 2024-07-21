39c73f5d1859b62c01426fade509ff3de3a82656
Jovil <jovilh@gmail.com>
2024-07-21 21:13:02 +0800

    ux

--- src/pages/UserShopPage/HeaderSection.tsx
+++ src/pages/UserShopPage/HeaderSection.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { NavLink } from "react-router-dom";
 import { useUser } from "../../context/UserContext";
 import loading from "../../assets/images/loading.gif";
 import defaultAvatar from "../../assets/images/toon_6.png";
@@ -56,27 +57,32 @@ const HeaderSection = ({
         <div className="text-xs flex flex-col gap-4 items-center">
           <div className="flex flex-col gap-3 items-center">
             <p className="text-dark">Shop</p>
-            <div className="w-16 h-16 border border-dark/60 rounded shadow-md relative overflow-hidden">
-              {profileLoadingAvatar && (
+            <NavLink
+              className="flex flex-col gap-3 items-center"
+              to={`/user/${isProfile._id}`}
+            >
+              <div className="w-16 h-16 border border-dark/60 rounded shadow-md relative overflow-hidden">
+                {profileLoadingAvatar && (
+                  <img
+                    className="w-6 h-6 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -z-0"
+                    src={loading}
+                    alt=""
+                  />
+                )}
                 <img
-                  className="w-6 h-6 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -z-0"
-                  src={loading}
+                  className="object-cover aspect-square"
+                  src={
+                    profileLoadingAvatar
+                      ? defaultAvatar
+                      : isProfile.avatar
+                      ? isProfile.avatar
+                      : defaultAvatar
+                  }
                   alt=""
                 />
-              )}
-              <img
-                className="object-cover aspect-square"
-                src={
-                  profileLoadingAvatar
-                    ? defaultAvatar
-                    : isProfile.avatar
-                    ? isProfile.avatar
-                    : defaultAvatar
-                }
-                alt=""
-              />
-            </div>
-            <p className="font-medium">{isProfile.userName}</p>
+              </div>
+              <p className="font-medium">{isProfile.userName}</p>
+            </NavLink>
           </div>
           {userState._id === isProfile._id && (
             <>
