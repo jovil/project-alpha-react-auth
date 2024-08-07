460e34009b29d6d0ecf7604aaa51aa654551a54c
Jovil <jovilh@gmail.com>
2024-08-07 23:18:39 +0800

    add form to join waiting list

--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -1,14 +1,41 @@
 import { useState } from "react";
 import { marqueePeeps } from "../../utils/marqueePeeps";
 import loading from "../../assets/images/loading.gif";
+import { apiUrl, postFetchConfig } from "../../utils/fetchConfig";
 
 const Home = () => {
+  const [formData, setFormData] = useState<Record<string, string>>({
+    email: "",
+  });
+  const [showFormMessage, setShowFormMessage] = useState<string>();
   const [runShimmerAnimation, setRunShimmerAnimation] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
 
   const handleOnLoad = () => {
     setIsLoading(false);
     setRunShimmerAnimation(true);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setFormData(() => ({
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const url = `${apiUrl}/waiting_list`;
+    try {
+      const response = await fetch(url, postFetchConfig(formData));
+      const result = await response.json();
+
+      if (result.error) return setShowFormMessage(result.error);
+      if (response.ok) return setShowFormMessage("Thank you for signing up!");
+    } catch (error) {
+      console.log("error", error);
+    }
   };
 
   return (
@@ -19,15 +46,33 @@ const Home = () => {
             Community Platform for Creators to Share, Sell, and Shine
           </h1>
 
-          <form className="flex gap-2">
-            <input
-              className="bg-blue-900 border border-dark/40 p-3 py-2.5 rounded min-w-[280px]"
-              type="email"
-              placeholder="Email"
-            />
-            <button className="btn-primary" type="submit">
-              Join waitlist
-            </button>
+          <form
+            className="flex flex-col relative"
+            onSubmit={(e) => handleSubmit(e)}
+          >
+            <div className="flex gap-2">
+              <input
+                className="bg-blue-900 border border-dark/40 p-3 py-2.5 rounded min-w-[280px]"
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+              <button
+                className="btn-primary"
+                onSubmit={(e) => handleSubmit(e)}
+                type="submit"
+              >
+                Join waitlist
+              </button>
+            </div>
+            {showFormMessage && (
+              <p className="absolute -bottom-2 translate-y-full">
+                {showFormMessage}
+              </p>
+            )}
           </form>
         </section>
         <section>
